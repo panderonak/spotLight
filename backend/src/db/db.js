0a5d12ca825eafe143213ca9d46d194a2ebe61f2
panderonak <pandeyronak139@gmail.com>
2024-12-20 20:02:09 +0530

    fix: reorganize project files to address incorrect structure

--- backend/src/db/db.js
+++ backend/src/db/db.js
@@ -4,11 +4,11 @@ import { DB_NAME } from "../constants.js";
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${process.env.MONGODB_URI}/${DB_NAME}`
+      `${process.env.MONGODB_URI}/${DB_NAME}`,
     );
     // console.log(connectionInstance);
     console.log(
-      `\n MONGODB CONNECTED SUCCESSFULLY! DB HOST: ${connectionInstance.connection.host}`
+      `\n MONGODB CONNECTED SUCCESSFULLY! DB HOST: ${connectionInstance.connection.host}`,
     );
   } catch (error) {
     console.log("MONGODB CONNECTION ERROR:", error);
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,5 @@
 import dotenv from "dotenv";
 import connectDB from "./db/db.js";
-
 dotenv.config({
   path: "./env",
 });
