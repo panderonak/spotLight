b70256c54abb00e6998248258c0c59b7e7838db6
panderonak <pandeyronak139@gmail.com>
2024-12-23 12:49:24 +0530

    feat: integrate file upload functionality with Cloudinary and multer

---
+++ backend/src/middlewares/multer.middlewares.js
@@ -0,0 +1,25 @@
+import multer from 'multer';
+import path from 'path';
+
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+const month = String(currentDate.getMonth() + 1).padStart(2, '0');
+const day = String(currentDate.getDate()).padStart(2, '0');
+const formattedDate = `${year}-${month}-${day}`;
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './public/temp');
+  },
+  filename: function (req, file, cb) {
+    const ext = path.extname(file.originalname);
+    const uniqueSuffix = `${Date.now()}-${Math.floor(Math.random() * 1e9)}`;
+    const newFileName = `${file.fieldname}-${formattedDate}-${uniqueSuffix}${ext}`;
+    cb(null, newFileName);
+    console.log(file);
+  },
+});
+
+export default upload = multer({
+  storage,
+});
