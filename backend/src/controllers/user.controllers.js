431dfebf024a1b0c4d654523121d58aa532ef423
panderonak <pandeyronak139@gmail.com>
2024-12-28 12:04:59 +0530

    fix: replace  with  for removing refreshToken in user data

--- backend/src/controllers/user.controllers.js
+++ backend/src/controllers/user.controllers.js
@@ -159,7 +159,7 @@ const logOutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set: { refreshToken: undefined },
+      $unset: { refreshToken: 1 },
     },
     { new: true }
   );
