11f79a7f4e19c9627be384d573105171895ac3f9
panderonak <pandeyronak139@gmail.com>
2025-01-05 16:47:38 +0530

    feat: initialize React project with Vite and Tailwind CSS for the application’s frontend.

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-3xl bg-green-400 text-center m-4">Eren Yeager</h1>
+    </>
+  );
+}
+
+export default App;
