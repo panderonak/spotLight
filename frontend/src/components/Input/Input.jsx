6062b9ecb41ba7ee50fd5840f1cf8004e054038f
panderonak <pandeyronak139@gmail.com>
2025-01-09 21:19:27 +0530

    refactor: enhance Button and Input components with improved styling and structure.

--- frontend/src/components/Input/Input.jsx
+++ frontend/src/components/Input/Input.jsx
@@ -1,15 +1,14 @@
-// TODO: Improve this component. Add label and accept props to make it re-usable.
 import { forwardRef, useId } from "react";
 
 function Input({ label, type = "text", className = "", ...props }, ref) {
   const Id = useId();
 
   return (
-    <div class="mb-3">
+    <div className="mb-3">
       {label && (
         <label
           htmlFor={Id}
-          className="mb-1 inline-block pl-1 text-base font-light"
+          className="mb-3 inline-block pl-1 text-base font-normal text-[#2d2d2d]"
         >
           {label}
         </label>
@@ -21,7 +20,7 @@ function Input({ label, type = "text", className = "", ...props }, ref) {
         {...props}
         id={Id}
         placeholder="Enter your email"
-        className={`${className} w-full rounded-lg border-2 border-[#f5f7fa] bg-[#f5f7fa] px-5 py-2.5 text-base font-normal text-black outline-none duration-200 placeholder:font-light placeholder:text-[#858586] focus:border-black`}
+        className={`${className} w-full rounded-xl border-2 border-[#f5f7fa] bg-[#f5f7fa] px-5 py-2.5 text-base font-normal text-black outline-none duration-200 placeholder:font-light placeholder:text-[#7c7b7d] focus:border-black`}
       />
     </div>
   );
