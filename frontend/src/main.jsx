42f0b984e338195963c2b831573fa1f947978588
panderonak <pandeyronak139@gmail.com>
2025-01-13 20:54:47 +0530

    feat: Add route-based lazy loading and preloading for improved performance and user experience.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,53 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { lazy, StrictMode, Suspense, useEffect } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { Provider } from "react-redux";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { UserDetailsForm, SignUp } from "./components/index";
+import HomePage from "./pages/HomePage";
+const App = lazy(() => import("./App"));
+const UserDetailsForm = lazy(() =>
+  import("./components/SignUp/UserDetailsForm")
+);
+const SignUp = lazy(() => import("./components/SignUp/SignUp"));
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const preloadComponent = (component) => {
+  component();
+};
+
+const PreloadApp = () => {
+  useEffect(() => {
+    preloadComponent(() => import("./pages/HomePage"));
+  }, []);
+
+  return (
+    <Suspense fallback={<>Loading...</>}>
+      <App />
+    </Suspense>
+  );
+};
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Suspense fallback={<div>Loading...</div>}>
+      <Route path="/" element={<PreloadApp />}>
+        <Route path="home" element={<HomePage />} />
+      </Route>
+      <Route path="/sign-up" element={UserDetailsForm} />
+      <Route path="/sign-up/avatar" element={SignUp} />
+    </Suspense>
+  )
+);
+
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <StrictMode>
+      <RouterProvider router={router} />
+    </StrictMode>
+  </Provider>
+);
