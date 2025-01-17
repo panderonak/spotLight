cc0e02705caace116b59ce68da96a491103185fc
panderonak <pandeyronak139@gmail.com>
2025-01-17 19:36:34 +0530

    feat: Centralize like-related API paths with likeConfig for improved modularity and maintainability.

---
+++ frontend/src/config/like.config.js
@@ -0,0 +1,8 @@
+const likeConfig = {
+  likedVideosPath: String(import.meta.env.VITE_LIKED_VIDEOS_PATH),
+  commentLikeTogglePath: String(import.meta.env.VITE_COMMENT_LIKE_TOGGLE_PATH),
+  videoLikeTogglePath: String(import.meta.env.VITE_VIDEO_LIKE_TOGGLE_PATH),
+  postLikeTogglePath: String(import.meta.env.VITE_POST_LIKE_TOGGLE_PATH),
+};
+
+export default likeConfig;
