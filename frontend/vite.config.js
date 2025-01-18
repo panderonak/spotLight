55bd454f1665ac6741908612dc52ac229e281f68
panderonak <pandeyronak139@gmail.com>
2025-01-18 16:29:18 +0530

    feat: Centralize API base URL in authenticationConfig and update Vite proxy usage.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import authenticationConfig from "./src/config/authentication.config";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1": process.env.VITE_API_BASE_URL,
+      "/api/v1": `${authenticationConfig.baseURLPath}`,
     },
   },
   plugins: [react()],
