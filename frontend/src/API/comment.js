82748ebbc8e5c4eb3be55cd395c75787cfc100b5
panderonak <pandeyronak139@gmail.com>
2025-01-19 19:33:53 +0530

    feat: Add CommentService with methods for managing comments and dynamic API paths.

---
+++ frontend/src/API/comment.js
@@ -0,0 +1,92 @@
+import axios from "axios";
+import commentConfig from "../config/comment.config";
+import { data } from "autoprefixer";
+
+export class CommentService {
+  constructor() {
+    this.method = "POST";
+    this.URL = "/api/v1/users";
+    this.headers = {
+      accept: "applicaion/json",
+      "content-type": "application/json",
+    };
+  }
+
+  async commentPost({ videoId, content }) {
+    const options = {
+      method: this.method,
+      url: `${this.URL}${commentConfig.commentPostPath}/${videoId}`,
+      headers: this.headers,
+      data: { content },
+    };
+
+    try {
+      const { data } = await axios.request(options);
+      console.log(data);
+      if (data.success) {
+        console.log("Success");
+        return data;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async fetchVideoComments({ videoId }) {
+    const options = {
+      method: "GET",
+      url: `${this.URL}${commentConfig.videoCommentsPath}/${videoId}`,
+      headers: this.headers,
+    };
+
+    try {
+      const { data } = await axios.request(options);
+      if (data.success) {
+        console.log("Success");
+        return data;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async editComment({ commentId, updatedComment }) {
+    const options = {
+      method: this.method,
+      url: `${this.URL}${commentConfig.editCommentPath}/${commentId}`,
+      headers: this.headers,
+      data: { updatedComment },
+    };
+    try {
+      const { data } = await axios.request(options);
+      if (data.success) {
+        console.log("Success");
+        return data;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async deleteComment({ commentId }) {
+    const options = {
+      method: "DELETE",
+      url: `${this.URL}${commentConfig.removeCommentPath}/${commentId}`,
+      headers: this.headers,
+    };
+
+    try {
+      const { data } = await axios.request(options);
+      if (data.success) {
+        console.log("Success");
+        return data;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+
+const commentService = new CommentService();
+
+export default commentService;
