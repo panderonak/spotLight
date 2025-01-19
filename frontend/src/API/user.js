e4be613d6b1ff6d5f3b5ef0d99667ff94516e1ca
panderonak <pandeyronak139@gmail.com>
2025-01-19 16:55:01 +0530

    feat: Add functional methods to UserService for user-related API operations.

--- frontend/src/API/user.js
+++ frontend/src/API/user.js
@@ -1,4 +1,7 @@
 import axios from "axios";
+import userConfig from "../config/user.config";
+import { data } from "autoprefixer";
+import { use } from "react";
 
 export class UserService {
   constructor() {
@@ -10,15 +13,101 @@ export class UserService {
     };
   }
 
-  async updatePassword({ currentPassword, newPassword, confirmNewPassword }) {}
+  async updatePassword({ currentPassword, newPassword, confirmNewPassword }) {
+    const options = {
+      method: this.method,
+      url: `${this.URL}${userConfig.updatePasswordPath}`,
+      headers: this.headers,
+      data: {
+        currentPassword,
+        newPassword,
+        confirmNewPassword,
+      },
+    };
 
-  async updateUserInfo({ fullname, email }) {}
+    try {
+      const { data } = await axios.request(options);
+      console.log(data);
+      if (data.success) {
+        console.log("Success");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-  async changeAvatar({ avatar }) {}
+  async updateUserInfo({ fullname, email }) {
+    const options = {
+      method: `PATCH`,
+      url: `${this.URL}${userConfig.updateUserPath}`,
+      headers: this.headers,
+      data: {
+        fullname,
+        email,
+      },
+    };
 
-  async changeCover({ coverImage }) {}
+    try {
+      const { data } = await axios.request(options);
+      if (data.success) {
+        console.log("Success");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-  async getWatchHistory() {}
+  async changeAvatar({ avatar }) {
+    const options = {
+      method: this.method,
+      url: `${this.URL}${userConfig.updateAvatarPath}`,
+      headers: this.headers,
+      data: {
+        avatar,
+      },
+    };
+    try {
+      const { data } = await axios.request(options);
+      if (data.success) console.log("Success");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async changeCover({ coverImage }) {
+    const options = {
+      method: this.method,
+      url: `${this.URL}${userConfig.updateCoverPath}`,
+      headers: this.headers,
+      data: {
+        coverImage,
+      },
+    };
+    try {
+      const { data } = await axios.request(options);
+      if (data.success) console.log("Success");
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async getWatchHistory() {
+    const options = {
+      method: `GET`,
+      url: `${this.URL}${userConfig.watchHistoryPath}`,
+      headers: this.headers,
+    };
+
+    try {
+      const { data } = await axios.request(options);
+      if (data.success) {
+        console.log("Success");
+        return data;
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
 }
 
 const userService = new UserService();
