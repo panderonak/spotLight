fa46b61a9a73639030544d21c9d525928be3c349
panderonak <pandeyronak139@gmail.com>
2025-01-22 20:28:35 +0530

    feat(auth-service): add token refresh mechanism and centralize API path configuration.

--- frontend/src/API/authentication.js
+++ frontend/src/API/authentication.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import authenticationConfig from "../config/authentication.config";
 
 export class AuthService {
   constructor() {
@@ -8,6 +9,35 @@ export class AuthService {
       accept: "application/json",
       "content-type": "application/json",
     };
+    this.refreshToken = this.refreshToken.bind(this);
+
+    axios.interceptors.response.use(
+      (response) => response,
+      async (error) => {
+        console.log(error);
+        const originalRequest = error.config;
+
+        if (error.response?.status === 401 && !originalRequest._retry) {
+          originalRequest._retry = true;
+
+          try {
+            const response = await this.refreshToken();
+
+            if (response.success) {
+              console.log("Token refreshed successfully");
+              return axios(originalRequest);
+            } else {
+              console.error("Token refresh failed. Please log in again.");
+              return Promise.reject(error);
+            }
+          } catch (error) {
+            console.error("Error refreshing token:", error);
+            return Promise.reject(error);
+          }
+        }
+        return Promise.reject(error);
+      }
+    );
   }
 
   async createAccount({ fullname, username, email, avatar, password }) {
@@ -20,7 +50,7 @@ export class AuthService {
 
     const options = {
       method: this.method,
-      url: `${this.URL}${process.env.VITE_USERS_REGISTER_PATH}`,
+      url: `${this.URL}${authenticationConfig.registerPath}`,
       data: formData,
     };
 
@@ -36,7 +66,7 @@ export class AuthService {
   async loginUser({ username, password }) {
     const options = {
       method: this.method,
-      url: `${this.URL}${process.env.VITE_USERS_LOGIN_PATH}`,
+      url: `${this.URL}${authenticationConfig.loginPath}`,
       headers: this.headers,
       data: {
         username: username?.trim(),
@@ -46,36 +76,64 @@ export class AuthService {
     try {
       const { data } = await axios.request(options);
       console.log(data);
+      return data;
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
 
   async getCurrentUser() {
     const options = {
       method: "GET",
-      url: `${this.URL}${process.env.VITE_USERS_CURRENT_USER_PATH}`,
+      url: `${this.URL}${authenticationConfig.currentUserPath}`,
       headers: { accept: "application/json" },
     };
     try {
       const { data } = await axios.request(options);
       console.log(data);
+      if (data.success) {
+        return data;
+      }
     } catch (error) {
-      console.log(error);
+      console.error(
+        "Service :: getCurrentUser - Error retrieving current user. Details: ",
+        error
+      );
     }
   }
 
   async logoutUser() {
     const options = {
       method: this.method,
-      url: `${this.URL}${process.env.VITE_USERS_LOGOUT_PATH}`,
+      url: `${this.URL}${authenticationConfig.logoutPath}`,
       headers: { accept: "application/json" },
     };
     try {
       const { data } = await axios.request(options);
       console.log(data);
     } catch (error) {
-      console.log(error);
+      console.error(
+        "Service :: logoutUser - Error during user logout process. Details: ",
+        error
+      );
+    }
+  }
+
+  async refreshToken() {
+    const options = {
+      method: this.method,
+      url: `${this.URL}${authenticationConfig.refreshTokenPath}`,
+      headers: { ...this.headers, withCredentials: true },
+    };
+    try {
+      const { data } = await axios.request(options);
+      if (data.success)
+        console.log("Successfully retrieved a new refresh token.");
+    } catch (error) {
+      console.error(
+        "Service :: refreshToken :: An error occurred while attempting to refresh the authentication token. Details: ",
+        error
+      );
     }
   }
 }
