fa26975a7537175150ae491ab8d58eac6c8f60e2
panderonak <pandeyronak139@gmail.com>
2025-01-22 15:38:54 +0530

    chore: remove unused import for cleanup.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,5 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import authenticationConfig from "./src/config/authentication.config";
 
 const baseURLPath = process.env.VITE_API_BASE_URL;
 
