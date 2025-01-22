d4e262aa59ec1051dae403cd236ca6fe017726e9
panderonak <pandeyronak139@gmail.com>
2025-01-22 15:23:40 +0530

    fix: correctly access environment variable in Vite config for Node.js runtime Access environment variables using process.env.VITE_API_BASE_URL as vite.config.js operates in a Node.js environment, not the browser. This ensures proper handling of the configuration.

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import authenticationConfig from "./src/config/authentication.config";
+// import authenticationConfig from "./src/config/authentication.config";
+
+const baseURLPath = process.env.VITE_API_BASE_URL;
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      "/api/v1": `${authenticationConfig.baseURLPath}`,
+      "/api/v1": baseURLPath,
     },
   },
   plugins: [react()],
