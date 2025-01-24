924932fba17959cc558205bb6fef4439b9276111
panderonak <pandeyronak139@gmail.com>
2025-01-24 18:50:35 +0530

    feat(home-page): setup base structure with responsive grid layout and dark theme.

--- frontend/src/pages/HomePage/HomePage.jsx
+++ frontend/src/pages/HomePage/HomePage.jsx
@@ -1,3 +1,11 @@
+import { VideoCard } from "../../components";
+
 export default function HomePage() {
-  return <div>HomePage</div>;
+  return (
+    <div className="h-screen overflow-y-auto bg-[#121212] text-white">
+      <section className="w-full pb-[70px] sm:ml-[70px] sm:pb-0 lg:ml-0">
+        <div className="grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] gap-4 p-4"></div>
+      </section>
+    </div>
+  );
 }
