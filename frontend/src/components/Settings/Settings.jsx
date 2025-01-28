ffda69dc56a4d4b62e46013dcc812f34a1159134
panderonak <pandeyronak139@gmail.com>
2025-01-28 19:38:48 +0530

    feat: replace buttons with NavLink in Settings for dynamic routing and improve UI/UX.

--- frontend/src/components/Settings/Settings.jsx
+++ frontend/src/components/Settings/Settings.jsx
@@ -1,4 +1,5 @@
-import { Outlet } from "react-router-dom";
+import { isAction } from "@reduxjs/toolkit";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function Settings() {
   return (
@@ -6,23 +7,49 @@ export default function Settings() {
       <h2 className="mb-12 text-left text-4xl leading-tight font-bold">
         Settings
       </h2>
-      <header className="sticky inset-x-0 top-0 z-50 w-full rounded-3xl bg-[#f7f7f7]">
+      <header className="sticky inset-x-0 top-0 z-50 w-full rounded-3xl bg-[#f7f7f7] mb-5">
         <nav className="mx-auto flex max-w-7xl items-center bg-[#fff] transition-all duration-200">
-          <ul className="no-scrollbar sticky top-[66px] z-[2] flex w-full flex-row items-center gap-x-2 overflow-auto rounded-3xl bg-[#f7f7f7] px-2 py-1.5 sm:top-[82px]">
-            <li className="w-full rounded-3xl shadow-sm drop-shadow-sm">
+          <ul className="no-scrollbar sticky top-[66px] z-[2] flex w-full flex-row items-center gap-x-2 overflow-auto rounded-[60px] bg-[#f7f7f7] shadow-inner px-2 py-1.5 sm:top-[82px]">
+            {/* <li className="w-full rounded-3xl shadow-sm drop-shadow-sm">
               <button className="w-full rounded-3xl bg-white px-3 py-3 text-[#07061a]">
                 Account Settings
               </button>
+            </li> */}
+
+            <li className="w-full rounded-3xl">
+              <NavLink
+                to="/profile/settings/update-profile"
+                className={({ isActive }) =>
+                  `${
+                    isActive
+                      ? "text-[#07061a] bg-white shadow-sm drop-shadow-sm"
+                      : "text-gray-500"
+                  }
+                  w-full block text-center rounded-3xl bg-[#f7f7f7] hover:bg-white hover:text-[#07061a] px-3 py-3 transition-all duration-300 hover:shadow-sm hover:drop-shadow-sm text-base font-semibold`
+                }
+              >
+                Account Settings
+              </NavLink>
             </li>
-            <li className="w-full rounded-4xl">
-              <button className="w-full rounded-4xl px-3 py-3 text-[#07061a]">
+            <li className="w-full rounded-3xl">
+              <NavLink
+                to="/profile/settings/update-password"
+                className={({ isActive }) =>
+                  `${
+                    isActive
+                      ? "text-[#07061a] bg-white shadow-sm drop-shadow-sm"
+                      : "text-gray-500"
+                  }
+                  w-full block text-center rounded-3xl bg-[#f7f7f7] hover:bg-white hover:text-[#07061a] px-3 py-3 transition-all duration-300 hover:shadow-sm hover:drop-shadow-sm text-base font-semibold`
+                }
+              >
                 Security
-              </button>
+              </NavLink>
             </li>
           </ul>
         </nav>
       </header>
-      <div>
+      <div className="py-5">
         <Outlet />
       </div>
     </section>
