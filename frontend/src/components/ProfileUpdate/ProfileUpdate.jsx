6b7c31a119c580f16652b023e5025bf1ad3766df
panderonak <pandeyronak139@gmail.com>
2025-01-30 17:18:01 +0530

    refactor(profile-update): Integrate React Hook Form, Redux, and API for better state management and UX.

--- frontend/src/components/ProfileUpdate/ProfileUpdate.jsx
+++ frontend/src/components/ProfileUpdate/ProfileUpdate.jsx
@@ -1,62 +1,99 @@
-export default function ProfileUpdate() {
-  return (
-    <div class="m-16 w-full rounded-xl bg-[#FCFCFC] px-5 pt-10 shadow-sm drop-shadow-md sm:w-1/2 lg:w-2/3">
-      <div class="bg-red-400">
-        <div class="flex flex-wrap gap-y-4 p-4">
-          <div class="w-full lg:w-1/2 lg:pr-2">
-            <label for="fullname" class="mb-1 inline-block">
-              {" "}
-              Full Name{" "}
-            </label>
-            <input
-              type="text"
-              class="w-full rounded-xl border border-[#C8C8C8] bg-[#FCFCFC] px-5 py-2.5 text-base font-normal text-black duration-200 outline-none placeholder:font-light placeholder:text-[#7c7b7d] focus:border-black"
-              id="fullname "
-              placeholder="Enter your full name"
-              value="Mikasa Ackerman"
-            />
-          </div>
+import { useState } from "react";
+import { Input, Button } from "../../components";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import userService from "../../API/user";
 
-          <div class="w-full">
-            <label for="lastname" class="mb-1 inline-block">
-              {" "}
-              Email address{" "}
-            </label>
-            <div class="relative">
-              <div class="absolute top-1/2 left-3 h-5 w-5 -translate-y-1/2 text-gray-300">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="#000"
-                  stroke-width="1.5"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                >
-                  <rect width="20" height="16" x="2" y="4" rx="2" />
-                  <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7" />
-                </svg>
+export default function ProfileUpdate() {
+  const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const { register, reset, handleSubmit } = useForm();
+
+  const handleCancel = () => {
+    reset();
+    navigate("/");
+  };
+
+  const updateUserDetails = async (data) => {
+    console.log(data);
+    setMessage("");
+    setLoading(false);
+    try {
+      setLoading(true);
+      const userUpdateResponse = await userService.updateUserInfo(data);
+      if (userUpdateResponse.success) {
+        setMessage("Your details have been updated successfully.");
+      }
+    } catch (error) {
+      setMessage(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className="flex flex-col flex-wrap justify-center gap-y-4 bg-[#fff] py-4 w-full sm:flex-row mt-4">
+      <div className="w-full sm:w-1/2 lg:w-1/3 sm:pr-5">
+        <h3 className="mb-4 text-left text-3xl leading-tight font-semibold">
+          Personal Info
+        </h3>
+        <p className="text-gray-500">Update your photo and personal details.</p>
+      </div>
+      <div className="w-full rounded-lg bg-[#f9fbfc] shadow-sm drop-shadow-md sm:w-1/2 lg:w-2/3">
+        <form onSubmit={handleSubmit(updateUserDetails)}>
+          <div className="rounded-lg">
+            <div className="flex flex-wrap gap-y-4 p-8">
+              <div className="w-full">
+                <Input
+                  label="Full Name*"
+                  placeholder="Enter your name"
+                  {...register("fullname", {
+                    required: true,
+                  })}
+                />
               </div>
-              <input
-                type="email"
-                class="w-full rounded-lg border bg-transparent py-1.5 pr-2 pl-10"
-                id="lastname"
-                placeholder="Enter email address"
-                value="mikasa@eldia.com"
-              />
+
+              <div className="w-full">
+                <div>
+                  <Input
+                    label="Email*"
+                    placeholder="Enter your email"
+                    {...register("email", {
+                      required: true,
+                      validate: (value) => {
+                        const isValid =
+                          /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/g.test(
+                            value
+                          );
+                        if (!isValid) {
+                          setMessage(
+                            "Please provide a valid email address (e.g., example@domain.com)."
+                          );
+                        }
+                        return isValid;
+                      },
+                    })}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="flex items-center justify-end gap-4 px-8 pb-11 pt-4">
+              <Button
+                onClick={handleCancel}
+                className="border text-base hover:bg-black/5 bg-white !text-black"
+              >
+                Cancel
+              </Button>
+
+              <Button type="submit" className="px-4 hover:bg-[rgba(3,3,3,0.8)]">
+                Save changes
+              </Button>
             </div>
           </div>
-        </div>
-      </div>
-      <div class="flex items-center justify-end gap-4 p-4">
-        <button class="text-md inline-block rounded-lg border px-3.5 py-2 text-black shadow-md duration-75 hover:bg-white/10">
-          Cancel
-        </button>
-        <button class="text-md inline-block rounded-lg bg-black px-5 py-2.5 text-white shadow-md duration-75 hover:bg-[rgba(3,3,3,0.8)]">
-          Save changes
-        </button>
+        </form>
       </div>
     </div>
   );
