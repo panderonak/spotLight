1aa6986c15355a29ddbd47dde3c62d4fc9d057d0
panderonak <pandeyronak139@gmail.com>
2025-02-01 19:45:23 +0530

    feat: Add new components (PasswordUpdate, Settings, AvatarUpdater, CoverImageUpdater, UserDetailsUpdate) to exports.

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -5,5 +5,23 @@ import UserDetailsForm from "./SignUp/UserDetailsForm";
 import Container from "./Container/Container";
 import LogIn from "./LogIn/LogIn";
 import VideoCard from "./VideoCard/VideoCard";
+import PasswordUpdate from "./PasswordUpdate/PasswordUpdate";
+import Settings from "./Settings/Settings";
+import AvatarUpdater from "./AvatarUpdater/AvatarUpdater";
+import CoverImageUpdater from "./CoverImageUpdater/CoverImageUpdater";
+import UserDetailsUpdate from "./UserDetailsUpdate/UserDetailsUpdate";
 
-export { Input, Button, SignUp, UserDetailsForm, Container, LogIn, VideoCard };
+export {
+  Input,
+  Button,
+  SignUp,
+  UserDetailsForm,
+  Container,
+  LogIn,
+  VideoCard,
+  PasswordUpdate,
+  Settings,
+  AvatarUpdater,
+  CoverImageUpdater,
+  UserDetailsUpdate,
+};
