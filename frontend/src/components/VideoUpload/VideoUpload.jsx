d63c7cdf4f945c05a10357c56a952d7100a85529
panderonak <pandeyronak139@gmail.com>
2025-02-03 20:55:56 +0530

    refactor(VideoUpload): Add dynamic thumbnail preview, improve button reuse, and clean up event handling.

--- frontend/src/components/VideoUpload/VideoUpload.jsx
+++ frontend/src/components/VideoUpload/VideoUpload.jsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from "react";
-import { Input } from "../../components";
+import { Button, Input } from "../../components";
 import { useNavigate } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
@@ -34,9 +34,7 @@ export default function VideoUpload() {
 
   const [thumbnail, setThumbnail] = useState(null);
 
-  const [thumbnailPreview, setThumbnailPreview] = useState(
-    "https://picsum.photos/200"
-  );
+  const [thumbnailPreview, setThumbnailPreview] = useState("");
 
   const thumbnailFileInputRef = useRef(null);
 
@@ -44,6 +42,7 @@ export default function VideoUpload() {
     const image = evt.target.files;
     if (image.length > 0) {
       setThumbnail(image[0]);
+      setThumbnailPreview(URL.createObjectURL(image[0]));
       console.log("Image dropped:", image[0]);
     } else {
       setMessage("Please drop a valid image file");
@@ -54,6 +53,7 @@ export default function VideoUpload() {
     const image = e.dataTransfer.files;
     if (image.length > 0) {
       setThumbnail(image[0]);
+      setThumbnailPreview(URL.createObjectURL(image[0]));
       console.log("Image dropped:", image[0]);
     } else {
       setMessage("Please drop a valid image file");
@@ -62,43 +62,45 @@ export default function VideoUpload() {
 
   const { register, handleSubmit } = useForm();
 
-  const uplaodVideo = () => {};
+  const handleVideoUpload = (data) => {
+    console.log(data);
+    setMessage("");
+  };
 
   return (
-    <div class="h-full overflow-auto border bg-[#fff]">
-      <div class="flex items-center justify-between border-b p-4 bg-pink-400">
-        <h2 class="text-xl font-semibold">Upload Videos</h2>
-        <button class="group/btn mr-1 flex w-auto items-center gap-x-2 bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e]">
-          Save
-        </button>
+    <div className="h-full overflow-auto border bg-[#fff]">
+      <div className="flex items-center justify-between border-b p-4 bg-pink-400">
+        <h2 className="text-xl font-semibold">Upload Videos</h2>
+
+        <Button type="submit">Save</Button>
       </div>
-      <div class="mx-auto flex w-full max-w-3xl flex-col gap-y-4 p-4 bg-blue-400">
+      <div className="mx-auto flex w-full max-w-3xl flex-col gap-y-4 p-4 bg-blue-400">
         <div
-          class="w-full rounded-2xl border-2 border-dashed border-[#d1d3d4] px-4 py-12 text-center bg-orange-400"
+          className="w-full rounded-2xl border-2 border-dashed border-[#d1d3d4] px-4 py-12 text-center bg-orange-400"
           onClick={() => videoFileInputRef.current.click()}
           onDragOver={(evt) => evt.preventDefault()}
           onDrop={handleVideoDrop}
         >
-          <span class="mb-4 inline-block w-24 rounded-full bg-black p-5 text-white">
+          <span className="mb-4 inline-block w-24 rounded-full bg-black p-5 text-white">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
               viewBox="0 0 24 24"
-              stroke-width="1.5"
+              strokeWidth="1.5"
               stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
               ></path>
             </svg>
           </span>
-          <h6 class="mb-2 font-semibold">Upload video</h6>
-          <p class="mb-2">Click to upload or drag and drop</p>
-          <p class="text-gray-400">Maximum file size: 100 MB</p>
-          <p class="text-gray-400">
+          <h6 className="mb-2 font-semibold">Upload video</h6>
+          <p className="mb-2">Click to upload or drag and drop</p>
+          <p className="text-gray-400">Maximum file size: 100 MB</p>
+          <p className="text-gray-400">
             Your videos will be private untill you publish them
           </p>
           <input
@@ -113,64 +115,97 @@ export default function VideoUpload() {
         </div>
 
         <div
-          class="w-full rounded-2xl border-2 border-dashed border-[#d1d3d4] px-4 py-12 text-center"
+          className="relative h-80 w-full rounded-2xl border-2 border-dashed border-[#d1d3d4] text-center bg-red-400"
           onClick={() => thumbnailFileInputRef.current.click()}
           onDragOver={(evt) => evt.preventDefault()}
           onDrop={handleThumbnailDrop}
         >
-          <span class="mb-4 inline-block w-24 rounded-full bg-black p-5 text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-image-up"
+          <div className="absolute -z-20 h-full w-full bg-orange-400 text-center">
+            <span className="mb-4 inline-block w-24 rounded-full bg-black p-5 text-white">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="lucide lucide-image-up"
+              >
+                <path d="M10.3 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10l-3.1-3.1a2 2 0 0 0-2.814.014L6 21" />
+                <path d="m14 19.5 3-3 3 3" />
+                <path d="M17 22v-5.5" />
+                <circle cx="9" cy="9" r="2" />
+              </svg>
+            </span>
+            <h6 className="mb-2 font-semibold">Upload thumbnail</h6>
+            <p className="mb-2">Click to upload or drag and drop</p>
+            <p className="text-gray-400">Maximum file size: 10 MB</p>
+          </div>
+
+          <div className="absolute z-20 h-full rounded-2xl bg-pink-400 w-full">
+            <input
+              type="file"
+              name="thumbnail"
+              accept="image/jpeg, image/png"
+              className="hidden"
+              ref={thumbnailFileInputRef}
+              required="true"
+              onChange={handleThumbnailFileChange}
+            />
+            <img
+              src={`${thumbnailPreview}`}
+              className="rounded-2xl object-cover h-full w-full"
+            />
+          </div>
+        </div>
+        <form onSubmit={handleSubmit(handleVideoUpload)}>
+          <div className="w-full bg-green-400">
+            <Input
+              label="Title*"
+              placeholder="Enter video title here"
+              {...register("title", {
+                required: "Title is needed.",
+                minLength: {
+                  value: 50,
+                  message: "Title must be at least 50 characters long.",
+                },
+                maxLength: {
+                  value: 75,
+                  message: "Title cannot be longer than 75 characters.",
+                },
+              })}
+            />
+          </div>
+          <div className="w-full bg-green-400">
+            <label
+              htmlFor="desc"
+              className="mb-3 inline-block pl-1 text-base font-normal text-[#2d2d2d]"
             >
-              <path d="M10.3 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2v10l-3.1-3.1a2 2 0 0 0-2.814.014L6 21" />
-              <path d="m14 19.5 3-3 3 3" />
-              <path d="M17 22v-5.5" />
-              <circle cx="9" cy="9" r="2" />
-            </svg>
-          </span>
-          <h6 class="mb-2 font-semibold">Upload thumbnail</h6>
-          <p class="mb-2">Click to upload or drag and drop</p>
-          <p class="text-gray-400">Maximum file size: 10 MB</p>
-
-          <input
-            type="file"
-            name="thumbnail"
-            accept="image/jpeg, image/png"
-            className="hidden"
-            ref={thumbnailFileInputRef}
-            required="true"
-            onChange={handleThumbnailFileChange}
-          />
-        </div>
-
-        <div class="w-full bg-green-400">
-          <Input
-            label="Title*"
-            placeholder="Enter video title here"
-            {...register("title", {
-              minLength: 50,
-              maxLength: 75,
-              required: true,
-            })}
-          />
-        </div>
-        <div class="w-full bg-green-400">
-          <label for="desc" class="mb-1 inline-block">
-            Description
-            <sup>*</sup>
-          </label>
-          <textarea
-            id="desc"
-            class="h-40 w-full resize-none border bg-transparent px-2 py-1 outline-none"
-          ></textarea>
-        </div>
+              Description
+              <sup>*</sup>
+            </label>
+            <textarea
+              {...register("description", {
+                required: "Description is required",
+                minLength: {
+                  value: 80,
+                  message: "Description must be at least 80 characters",
+                },
+                maxLength: {
+                  value: 150,
+                  message: "Description cannot be longer than 150 characters",
+                },
+              })}
+              id="desc"
+              className="h-40 resize-none   
+            w-full rounded-xl border border-[#C8C8C8] bg-[#FCFCFC] px-5 py-2.5 text-base font-normal text-black outline-none duration-200 placeholder:font-light placeholder:text-[#7c7b7d] focus:border-black"
+            ></textarea>
+          </div>
+          <div>
+            <Button type="submit">Upload</Button>
+          </div>
+        </form>
       </div>
     </div>
   );
