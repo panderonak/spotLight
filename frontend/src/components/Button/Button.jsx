c54fc1a5063dedf53e589f30c82baff826f6ad35
panderonak <pandeyronak139@gmail.com>
2025-02-04 21:03:09 +0530

    fix(Button): correct syntax error in className template literal.

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -8,7 +8,7 @@ export default function Button({
 }) {
   return (
     <button
-      className={`text-md inline-block rounded-lg bg-black px-10 py-2.5 text-white shadow-md duration-75 hover:bg-[rgba(3,3,3,0.8)]{bgColor} ${textColor} ${className}
+      className={`text-md inline-block rounded-lg bg-black px-10 py-2.5 text-white shadow-md duration-75 hover:bg-[rgba(3,3,3,0.8)] {bgColor} ${textColor} ${className}
       `}
       {...props}
     >
