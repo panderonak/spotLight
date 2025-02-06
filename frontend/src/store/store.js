5860edf8da11d4ebaf70e739b6fc21b118f41a3a
panderonak <pandeyronak139@gmail.com>
2025-02-06 19:05:52 +0530

    feat: add video upload reducer to Redux store.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import registrationReducer from "../features/registrationSlice";
+import videoUploadReducer from "../features/videoUploadSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     registration: registrationReducer,
+    videoUploadState: videoUploadReducer,
   },
 });
 
