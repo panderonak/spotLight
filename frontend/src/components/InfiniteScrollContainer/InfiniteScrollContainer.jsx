e78001c65e22ea500c6ee15fa514a11833481a05
panderonak <pandeyronak139@gmail.com>
2025-02-09 20:08:36 +0530

    feat: Add infinite scrolling using Intersection Observer.

---
+++ frontend/src/components/InfiniteScrollContainer/InfiniteScrollContainer.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react";
+
+export default function InfiniteScrollContainer({
+  children,
+  fetchNextPageVideos,
+  hasNextPage,
+}) {
+  const bottomPageIndicatorRef = useRef(null);
+
+  useEffect(() => {
+    const bottomPageIndicatorElement = bottomPageIndicatorRef.current;
+    const observer = new IntersectionObserver((entries) => {
+      const target = entries[0];
+      if (target.isIntersecting && hasNextPage) {
+        fetchNextPageVideos();
+      }
+    });
+
+    if (bottomPageIndicatorElement)
+      observer.observe(bottomPageIndicatorElement);
+
+    return () => observer.unobserve(bottomPageIndicatorElement);
+  }, [fetchNextPageVideos, hasNextPage]);
+
+  return (
+    <>
+      {children}
+      <div ref={bottomPageIndicatorRef} className="h-12"></div>
+    </>
+  );
+}
