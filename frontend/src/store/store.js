f3903527a36831281043fb9590096202321f0a8b
panderonak <pandeyronak139@gmail.com>
2025-02-11 13:08:39 +0530

    feat: Add videos reducer to Redux store for managing video state.

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/authSlice";
 import registrationReducer from "../features/registrationSlice";
 import videoUploadReducer from "../features/videoUploadSlice";
+import videosReducer from "../features/videosSlice";
 
 const store = configureStore({
   reducer: {
     auth: authReducer,
     registration: registrationReducer,
     videoUpload: videoUploadReducer,
+    videos: videosReducer,
   },
 });
 
