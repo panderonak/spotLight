d14c7bf58db1aa55832cb61d1d24cf3edf07f42f
panderonak <pandeyronak139@gmail.com>
2025-02-16 20:26:32 +0530

    refactor: add children prop to CommentSection for flexible content rendering.

--- frontend/src/components/CommentSection/CommentSection.jsx
+++ frontend/src/components/CommentSection/CommentSection.jsx
@@ -1,4 +1,6 @@
-export default function CommentSection() {
+import { CommentCard } from "../../components";
+
+export default function CommentSection({ children }) {
   return (
     <div class="fixed inset-x-0 top-full z-[60] h-[calc(100%-69px)] overflow-auto rounded-lg border bg-teal-700 p-4 duration-200 peer-focus:top-[67px] hover:top-[67px] sm:static sm:h-auto sm:max-h-[500px] lg:max-h-none">
       <div class="block pb-10">
@@ -9,6 +11,16 @@ export default function CommentSection() {
           placeholder="Add a comment..."
         />
       </div>
+      // TODO: Apply loop here.
+      <CommentCard />
     </div>
   );
 }
+
+// TODO: Combine all parts to achieve the functionality.
+// Share on X that completed 55-60% project.
+
+// Algorithm:
+// 1) Use the videoId to fetch the video.
+// 2) Then fetch the comments.
+// 3)
