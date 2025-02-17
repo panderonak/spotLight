0e4ab6ba21c08ae3b3d3453938aac5c376eeb43d
panderonak <pandeyronak139@gmail.com>
2025-02-17 20:27:18 +0530

    feat: Integrated Redux user data and react-hook-form in CommentSection for dynamic comment input with user avatars.

--- frontend/src/components/CommentSection/CommentSection.jsx
+++ frontend/src/components/CommentSection/CommentSection.jsx
@@ -1,26 +1,34 @@
+import { useSelector } from "react-redux";
 import { CommentCard } from "../../components";
+import { useForm } from "react-hook-form";
 
 export default function CommentSection({ children }) {
+  const { register, handleSubmit } = useForm();
+  const userData = useSelector((state) => state.userInfo);
+  console.log(userData.user);
   return (
-    <div class="fixed inset-x-0 top-full z-[60] h-[calc(100%-69px)] overflow-auto rounded-lg border bg-teal-700 p-4 duration-200 peer-focus:top-[67px] hover:top-[67px] sm:static sm:h-auto sm:max-h-[500px] lg:max-h-none">
-      <div class="block pb-10">
-        <h6 class="mb-4 text-xl font-semibold">573 Comments</h6>
-        <input
-          type="text"
-          class="w-full border-b border-[#3f3f3f] bg-transparent py-1 font-normal outline-none placeholder:text-sm placeholder:text-[#aaaaaa]"
-          placeholder="Add a comment..."
-        />
+    <form onSubmit={handleSubmit()}>
+      <div class="fixed inset-x-0 top-full z-[60] h-[calc(100%-69px)] overflow-auto rounded-lg border bg-teal-700 p-4 duration-200 peer-focus:top-[67px] hover:top-[67px] sm:static sm:h-auto sm:max-h-[500px] lg:max-h-none">
+        <div class="block pb-10">
+          <h6 class="mb-4 text-xl font-semibold">573 Comments</h6>
+          <div class="flex items-center gap-x-4">
+            <span class="inline-block w-10 shrink-0 rounded-full sm:group-hover:mr-2 lg:mr-2">
+              <img
+                src="https://picsum.photos/200"
+                alt="Joe Doe Picture"
+                class="h-10 w-10 rounded-full"
+              />
+            </span>
+            <input
+              {...register("content", { required: true })}
+              type="text"
+              class="w-full border-b border-[#3f3f3f] bg-transparent py-1 font-normal outline-none placeholder:text-sm placeholder:text-[#aaaaaa]"
+              placeholder="Add a comment..."
+            />
+          </div>
+        </div>
+        <CommentCard />
       </div>
-      // TODO: Apply loop here.
-      <CommentCard />
-    </div>
+    </form>
   );
 }
-
-// TODO: Combine all parts to achieve the functionality.
-// Share on X that completed 55-60% project.
-
-// Algorithm:
-// 1) Use the videoId to fetch the video.
-// 2) Then fetch the comments.
-// 3)
