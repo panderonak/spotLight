b0f450e4412cb878cf2a6b8e363fa006cfe9ed6f
panderonak <pandeyronak139@gmail.com>
2025-02-28 16:54:46 +0530

    refactor: enhance CommentSection with infinite scrolling, modular components, and improved state management.

--- frontend/src/components/CommentSection/CommentSection.jsx
+++ frontend/src/components/CommentSection/CommentSection.jsx
@@ -1,35 +1,59 @@
 import { useSelector } from "react-redux";
-import { CommentCard } from "../../components";
+import {
+  CommentCard,
+  CommentInput,
+  InfiniteScrollContainer,
+} from "../../components";
 import { useForm } from "react-hook-form";
+import { createNewComment, fetchComments } from "../../features/commentSlice";
 
-export default function CommentSection({ children }) {
+export default function CommentSection({ videoId }) {
   const { register, handleSubmit } = useForm();
   const userData = useSelector((state) => state.userInfo);
   console.log(userData.user);
+  const [page, setPage] = useState(1);
+
+  const { isLoading, comments, totalCommentCount, hasMoreComments } =
+    useSelector((state) => state.comment);
+
+  const fetchMoreComments = useCallback(() => {
+    if (!isLoading && hasNextPage) {
+      dispatch(fetchComments({ videoId, page: page + 1 }));
+      setPage((prev) => prev + 1);
+    }
+  }, [page, isLoading, hasNextPage, dispatch, videoId]);
+
+  const handleCommentSubmit = (data) => {
+    const response = createNewComment();
+  };
+
   return (
-    <form onSubmit={handleSubmit()}>
+    <form onSubmit={handleSubmit(handleCommentSubmit)}>
       <div class="fixed inset-x-0 top-full z-[60] h-[calc(100%-69px)] overflow-auto rounded-lg border bg-teal-700 p-4 duration-200 peer-focus:top-[67px] hover:top-[67px] sm:static sm:h-auto sm:max-h-[500px] lg:max-h-none">
         <div class="block pb-10">
-          <h6 class="mb-4 text-xl font-semibold">573 Comments</h6>
+          <h6 class="mb-4 text-xl font-semibold">{totalCommentCount}</h6>
           <div class="flex items-center gap-x-4">
             <span class="inline-block w-10 shrink-0 rounded-full sm:group-hover:mr-2 lg:mr-2">
               <img
-                src="https://picsum.photos/200"
-                alt="Joe Doe Picture"
+                src={`userData?.user?.avatar`}
+                alt={`userData.user.username`}
                 class="h-10 w-10 rounded-full"
               />
             </span>
-            <input
+            <CommentInput
               {...register("content", { required: true })}
-              type="text"
-              class="w-full border-b border-[#3f3f3f] bg-transparent py-1 font-normal outline-none placeholder:text-sm placeholder:text-[#aaaaaa]"
               placeholder="Add a comment..."
             />
           </div>
         </div>
-        {comments?.map((comment) => (
-          <CommentCard key={comment._id} content={comment.content} />
-        ))}
+        <InfiniteScrollContainer
+          fetchNextPage={fetchMoreComments}
+          hasNextPage={hasMoreComments}
+        >
+          {comments?.map((comment) => (
+            <CommentCard key={comment?._id} content={comment?.content} />
+          ))}
+        </InfiniteScrollContainer>
       </div>
     </form>
   );
