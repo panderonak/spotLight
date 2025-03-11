5fae05aafbb28a0d48ea50e15d95fd0a29638467
panderonak <pandeyronak139@gmail.com>
2025-03-11 20:19:17 +0530

    feat: Add ChannelBanner with responsive cover image and styling.

---
+++ frontend/src/components/ChannelBanner/ChannelBanner.jsx
@@ -0,0 +1,13 @@
+export default function ChannelBanner() {
+  return (
+    <div class="relative min-h-[200px] w-full pt-[16.28%] bg-amber-600">
+      <div class="absolute inset-0 overflow-hidden p-2">
+        <img
+          class="h-full w-full rounded-2xl object-cover"
+          src="https://images.pexels.com/photos/30267350/pexels-photo-30267350/free-photo-of-charming-breton-house-with-bicycle-in-ouessant.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
+          alt="cover-photo"
+        />
+      </div>
+    </div>
+  );
+}
