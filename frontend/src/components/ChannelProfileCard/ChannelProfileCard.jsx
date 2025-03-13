68617285bb162fba7a060533946ee2f8c7448c82
panderonak <pandeyronak139@gmail.com>
2025-03-13 19:06:53 +0530

    refactor(ChannelProfileCard): Replace static profile details with dynamic data, manage subscriptions, and improve UI.

--- frontend/src/components/ChannelProfileCard/ChannelProfileCard.jsx
+++ frontend/src/components/ChannelProfileCard/ChannelProfileCard.jsx
@@ -1,29 +1,81 @@
-export default function ChannelProfileCard() {
+import userService from "../../API/user";
+
+export default function ChannelProfileCard({ username }) {
+  const [channelProfile, setChannelProfile] = useState({});
+
+  useEffect(() => {
+    const subscription = async () => {
+      try {
+        const channelInfo = await userService.getChannelProfile({ username });
+        if (channelInfo.success) setChannelProfile(channelInfo?.data);
+      } catch (error) {
+        console.error(error?.message);
+      }
+    };
+
+    return () => {
+      subscription.unsubscribe();
+    };
+  }, [third]);
+
+  const [hasSubscription, setHasSubscription] = useState(
+    channelProfile?.isSubscribed
+  );
+
+  const toggleSubscriptionStatus = async ({ channelId }) => {
+    try {
+      const response = await subscriptionService.toggleChannelSubscription({
+        channelId,
+      });
+
+      if (response?.success) {
+        console.log("You have successfully subscribed to the channel!");
+        setHasSubscription((prev) => !prev);
+      } else {
+        console.log("Failed to subscribe to the channel. Please try again.");
+      }
+    } catch (error) {
+      console.error(
+        "An error occurred while subscribing to the channel:",
+        error?.message || error
+      );
+    }
+  };
+
   return (
     <div class="flex flex-wrap gap-4 bg-amber-200 pt-6 pb-4">
       <span class="relative inline-block h-35 w-35 shrink-0 overflow-hidden rounded-full">
-        <img
-          src="https://images.pexels.com/photos/30724332/pexels-photo-30724332/free-photo-of-woman-embracing-calm-outdoors.jpeg?auto=compress&cs=tinysrgb&w=1200&lazy=load"
-          alt="Channel"
-          class="h-full w-full"
-        />
+        <img src={channelProfile?.avatar} alt="Channel" class="h-full w-full" />
       </span>
       <div class="mr-auto">
         <div class="flex h-full w-full flex-col justify-center">
-          <h1 class="pb-2 text-4xl font-bold text-white">The Eren Yeager</h1>
+          <h1 class="pb-2 text-4xl font-bold text-white">
+            {channelProfile?.fullname}
+          </h1>
           <div class="flex items-center gap-x-2">
-            <p class="text-sm font-medium text-white">@TheErenYeager</p>
+            <p class="text-sm font-medium text-white">
+              {`@${channelProfile?.username}`}
+            </p>
             <p class="text-sm font-normal text-[#aaaaaa]">
-              • 800M subscribers • 13 videos
+              • {channelProfile?.subscribersCount}
             </p>
           </div>
         </div>
       </div>
       <div class="inline-block">
         <div class="inline-flex h-full min-w-[145px] items-center justify-start">
-          <button class="group/btn mr-1 flex w-full items-center gap-x-2 bg-[#ae7aff] px-3 py-2 text-center font-bold text-black shadow-[5px_5px_0px_0px_#4f4e4e] transition-all duration-150 ease-in-out active:translate-x-[5px] active:translate-y-[5px] active:shadow-[0px_0px_0px_0px_#4f4e4e] sm:w-auto">
-            <span class="group-focus/btn:hidden">Subscribe</span>
-            <span class="hidden group-focus/btn:block">Subscribed</span>
+          <button
+            onClick={() =>
+              toggleSubscriptionStatus({ channelId: channelProfile?._id })
+            }
+            className={`
+              ${
+                hasSubscription
+                  ? "bg-[#f1f1f1] hover:bg-[#f1f1f1]/80 text-[#272727]"
+                  : "bg-[#272727] hover:bg-[#272727]/80 text-[#f1f1f1]"
+              } group/btn w-full rounded-3xl  px-4 py-2 text-center text-sm font-medium transition-all duration-200 ease-in-out  sm:w-auto cursor-pointer`}
+          >
+            <span>{hasSubscription ? "Subscribed" : "Subscribe"}</span>
           </button>
         </div>
       </div>
