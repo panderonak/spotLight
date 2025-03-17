664a223c474b380523e363e356c1553bed79ce85
panderonak <pandeyronak139@gmail.com>
2025-03-17 16:19:47 +0530

    feat(PlaylistCard): Improve performance with memo and lazy loading, enhance UI.

--- frontend/src/components/PlaylistCard/PlaylistCard.jsx
+++ frontend/src/components/PlaylistCard/PlaylistCard.jsx
@@ -1,23 +1,28 @@
-export default function PlaylistCard({
+import { memo } from "react";
+
+function PlaylistCard({
   playlistTitle,
   playlistDescription,
   thumbnailImage,
   totalVideos,
 }) {
   return (
-    <div class="w-full p-0.5">
-      <div class="relative mb-2 w-full pt-[56%]">
-        <div class="absolute inset-0">
-          <img
-            src={`${thumbnailImage}`}
-            alt={`${playlistTitle} Playlist`}
-            class="h-full w-full rounded-2xl object-cover"
-          />
-          <div class="absolute inset-x-0 bottom-0">
-            <div class="relative px-2 py-2">
-              <div class="relative z-[1]">
-                <p class="flex justify-end">
-                  <span class="flex items-center justify-center gap-1.5 rounded-md bg-[rgba(0,0,0,0.4)] px-1.5 py-1 text-white">
+    <div className="w-full p-0.5">
+      <div className="relative mb-2 w-full pt-[56%]">
+        <div className="absolute inset-0">
+          <div className="h-full w-full rounded-2xl bg-gray-700">
+            <img
+              src={thumbnailImage}
+              alt={`${playlistTitle} Playlist`}
+              className="h-full w-full rounded-2xl object-cover"
+              loading="lazy"
+            />
+          </div>
+          <div className="absolute inset-x-0 bottom-0">
+            <div className="relative px-2 py-2">
+              <div className="relative z-[1]">
+                <p className="flex justify-end">
+                  <span className="flex items-center justify-center gap-1.5 rounded-md bg-[rgba(0,0,0,0.4)] px-1.5 py-1 text-white">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       width="18"
@@ -25,16 +30,16 @@ export default function PlaylistCard({
                       viewBox="0 0 24 24"
                       fill="none"
                       stroke="currentColor"
-                      stroke-width="2"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
                       <path d="M12 12H3" />
                       <path d="M16 6H3" />
                       <path d="M12 18H3" />
                       <path d="m16 12 5 3-5 3v-6Z" />
                     </svg>
-                    <span class="inline-block text-sm font-semibold">
+                    <span className="inline-block text-sm font-semibold">
                       {`${totalVideos} videos`}
                     </span>
                   </span>
@@ -44,8 +49,10 @@ export default function PlaylistCard({
           </div>
         </div>
       </div>
-      <h6 class="mb-1 font-semibold text-white">{playlistTitle}</h6>
-      <p class="flex text-sm text-gray-200">{playlistDescription}</p>
+      <h6 className="mb-1 font-semibold text-white">{playlistTitle}</h6>
+      <p className="flex text-sm text-gray-200">{playlistDescription}</p>
     </div>
   );
 }
+
+export default memo(PlaylistCard);
