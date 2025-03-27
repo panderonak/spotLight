48a34b9e8c6e256ae5be046c556f45da4d3973cd
panderonak <pandeyronak139@gmail.com>
2025-03-27 15:44:21 +0530

    fix: add missing React import for proper JSX handling.

--- frontend/src/components/CommentCard/CommentCard.jsx
+++ frontend/src/components/CommentCard/CommentCard.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useState, useCallback } from "react";
 import { deleteComment, updateComment } from "../../features/commentSlice";
