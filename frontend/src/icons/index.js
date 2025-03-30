090c7aaf7e2736d7fcf155f012b966e236c8c2e7
panderonak <pandeyronak139@gmail.com>
2025-03-30 17:10:40 +0530

    refactor: extract SVG icons into reusable components for cleaner and more maintainable code.

---
+++ frontend/src/icons/index.js
@@ -0,0 +1,29 @@
+import { ProjectorIcon } from "./ProjectorIcon";
+import { SearchIcon } from "./SearchIcon";
+import { MenuIcon } from "./MenuIcon";
+import { CloseIcon } from "./CloseIcon";
+import { LikedIcon } from "./LikedIcon";
+import { HistoryIcon } from "./HistoryIcon";
+import { ContentIcon } from "./ContentIcon";
+import { PlaylistIcon } from "./PlaylistIcon";
+import { SubscriptionsIcon } from "./SubscriptionsIcon";
+import { SupportIcon } from "./SupportIcon";
+import { SettingsIcon } from "./SettingsIcon";
+import { LogoutIcon } from "./LogoutIcon";
+import { HomeIcon } from "./HomeIcon";
+
+export {
+  ProjectorIcon,
+  SearchIcon,
+  MenuIcon,
+  CloseIcon,
+  HomeIcon,
+  LikedIcon,
+  HistoryIcon,
+  ContentIcon,
+  PlaylistIcon,
+  SubscriptionsIcon,
+  SupportIcon,
+  SettingsIcon,
+  LogoutIcon,
+};
